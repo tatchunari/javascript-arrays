f0b70397b2fb1355a2e668d42f45b63e137211fc
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-07-17 13:55:46 +0700

    complete exercise 1-7 in Arrays

--- ex-4.js
+++ ex-4.js
@@ -12,3 +12,15 @@ const employees = [
 ];
 
 // Start coding here
+employees.push({
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading Novels"],
+}, {
+  name: "Cody",
+  age: 19,
+  hobbies: ["Computer games", "Wakeboard"],
+});
+
+console.log(employees);
+
